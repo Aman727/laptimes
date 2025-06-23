5fab9e3ac3063b35ed768f552047de169333c669
aman727 <aman.ahmed86@icloud.com>
2025-06-23 21:09:54 +0100

    Initial commit

---
+++ src/main/java/com/example/laptimes/LaptimesApplication.java
@@ -0,0 +1,13 @@
+package com.example.laptimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptimesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaptimesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/laptimes/LaptimesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laptimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptimesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
