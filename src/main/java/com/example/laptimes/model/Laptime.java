2a9c1e58651abbd865e7c66ba8f469c955c8e0ab
aman727 <aman.ahmed86@icloud.com>
2025-06-24 20:35:37 +0100

    Added Laptime.java model

---
+++ src/main/java/com/example/laptimes/model/Laptime.java
@@ -0,0 +1,9 @@
+package com.example.laptimes.model;
+
+public class Laptime {
+    private int position;
+    private String username;
+    private String circuit;
+    private String car;
+    private String time;
+}
